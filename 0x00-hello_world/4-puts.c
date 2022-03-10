55e1004a9e7996737fbc0195815195048e1e0ad9
Nabil Affo <nabil.noble01@gmail.com>
2022-03-10 15:22:22 +0000

    Update script 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+/**
+ *Main program: Print the phrase "Programming is like building a multilingual puzzle
+ *
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
