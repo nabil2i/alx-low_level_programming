9e5f27ab50d576679ae4cf15628e79850de3ea93
Nabil Affo <nabil.noble01@gmail.com>
2022-03-11 13:57:53 +0000

    Write script 5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 /**
- * main - print all single digit numbers of base 10 
+ * main - print all single digit numbers of base 10
  * starting with 0
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int i;
+
 	i = 0;
 	while (i < 10)
 	{
