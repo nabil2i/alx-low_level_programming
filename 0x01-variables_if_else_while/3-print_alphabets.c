96ca0e318e26ec0d00684caa8845210233575a2b
Nabil Affo <nabil.noble01@gmail.com>
2022-03-11 11:51:31 +0000

    Write script 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - print the alphabet in lowercase, then in uppercase 
+ * followed by a newline
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while (c < 123)
+	{
+		putchar(c);
+		c++;
+	}
+	c = 'A';
+	while (c < 91)
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
