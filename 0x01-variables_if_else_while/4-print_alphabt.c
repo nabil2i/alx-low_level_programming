1694fb0e2508e2a6fdce798b438a9033dded2a76
Nabil Affo <nabil.noble01@gmail.com>
2022-03-11 12:17:30 +0000

    Update script 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-/**main - printthe alphabet in lowercase followed
+/**
+ * main - printthe alphabet in lowercase followed
  * by a new line, donb't print 'e' and 'q'
  * Return: Always 0 (Success)
  */
@@ -8,10 +9,17 @@ int main(void)
 	char c;
 
 	c = 'a';
-	while (c < 123 && c != 'e' && c != 'q')
-	{
-		putchar(c);
-		c++;
+	while (c < 123)
+	{	if (c != 'e' && c != 'q')
+		{	
+			putchar(c);
+			c++;	
+		
+		}
+		else
+		{
+			c++;
+		}
 	}
 	putchar('\n');
 	return (0);
