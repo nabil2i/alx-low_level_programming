7f91d6a3ca2809fe71d91e898d678a5d51f5de55
Nabil Affo <nabil.noble01@gmail.com>
2022-03-11 15:18:11 +0000

    Write script 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - print all possible combination of single digit
+ * numbers
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	i = 48;
+	for (i = 48; i < 58; i++)
+	{	
+		putchar(i);
+		if (i != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}	
+	putchar('\n');
+	return (0);
+}
