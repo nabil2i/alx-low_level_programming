56cf7388cb84b44a15596b6cb5039c1677f39851
Nabil Affo <nabil.noble01@gmail.com>
2022-03-11 11:09:22 +0000

    Write script 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - the alphabet game
+ * print the alphabet with putchar
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while (c<123)
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
