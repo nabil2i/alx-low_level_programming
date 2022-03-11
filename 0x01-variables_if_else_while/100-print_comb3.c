63132cdf496e2eb6f54345de43fd33d1344aeffc
Nabil Affo <nabil.noble01@gmail.com>
2022-03-11 16:17:05 +0000

    Write scripts 10 and 11

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - print all possible different combinations of two digits
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 48; i < 57; i++)
+	{	
+		for (j = 49; j < 58; j++)
+		{	if (j > i)
+			{
+				putchar(i);
+				putchar(j);
+				if (i != 56 || j !=57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main - print all possible different combinations of two digits
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        int i;
+        int j;
+	int k;
+
+        for (i = 48; i < 57; i++)
+        {
+                for (j = 49; j < 58; j++)
+                {       	
+			for (k = 50; k < 58; k++)
+			{
+				if (j > i && k > j)
+                        	{
+                                	putchar(i);
+                                	putchar(j);
+					putchar(k);
+                                	if (i != 55 || j !=56 || k != 57)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+                }
+        }
+        putchar('\n');
+        return (0);
+}
