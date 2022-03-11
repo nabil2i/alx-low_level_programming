0a188f28cc62701fa55b89f42a867a2cd42dcf29
Nabil Affo <nabil.noble01@gmail.com>
2022-03-11 16:20:06 +0000

    Update script 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -5,30 +5,30 @@
  */
 int main(void)
 {
-        int i;
-        int j;
+	int i;
+	int j;
 	int k;
-
-        for (i = 48; i < 57; i++)
-        {
-                for (j = 49; j < 58; j++)
-                {       	
+	
+	for (i = 48; i < 57; i++)
+	{
+		for (j = 49; j < 58; j++)
+		{       	
 			for (k = 50; k < 58; k++)
 			{
 				if (j > i && k > j)
-                        	{
-                                	putchar(i);
-                                	putchar(j);
+				{
+					putchar(i);
+					putchar(j);
 					putchar(k);
-                                	if (i != 55 || j !=56 || k != 57)
+					if (i != 55 || j !=56 || k != 57)
 					{
 						putchar(',');
 						putchar(' ');
 					}
 				}
 			}
-                }
-        }
-        putchar('\n');
-        return (0);
+		}
+	}
+	putchar('\n');
+	return (0);
 }
