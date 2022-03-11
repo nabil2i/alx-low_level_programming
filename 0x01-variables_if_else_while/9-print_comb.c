8f898001457699298f232ff77849dd481271db3b
Nabil Affo <nabil.noble01@gmail.com>
2022-03-11 15:19:52 +0000

    Update script 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,14 +10,14 @@ int main(void)
 
 	i = 48;
 	for (i = 48; i < 58; i++)
-	{	
+	{
 		putchar(i);
 		if (i != 57)
 		{
 			putchar(',');
 			putchar(' ');
 		}
-	}	
+	}
 	putchar('\n');
 	return (0);
 }
