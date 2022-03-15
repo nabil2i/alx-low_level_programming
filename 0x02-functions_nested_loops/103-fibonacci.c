fe47eb805fc7cf27ecb7ddc5b5c4c1d59d1bac70
Nabil Affo <nabil.noble01@gmail.com>
2022-03-15 21:23:37 +0000

    Update script 15

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - find and print the sum of even-valued terms of Fibonacci
- * sequence and the max value of the Fibonacci sequence 
+ * sequence and the max value of the Fibonacci sequence
  * should be 4 000 000
  * Return: Always 0 (Success)
  */
@@ -11,16 +11,16 @@ int main(void)
 	unsigned long int m, n, nextterm, sum;
 
 	sum = 0;
-	while(i <= 33);
+	while (i <= 33);
 	{
-		if(n < 4000000 && (m % 2) == 0)
+		if (n < 4000000 && (m % 2) == 0)
 		{
 			sum = sum + m;
 		}
 		nextterm = m + n;
 		m = n;
 		n = nextterm;
-
+		i++;
 	}
 	printf("%lu\n", sum);
 	return (0);
