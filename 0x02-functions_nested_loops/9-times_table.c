3ac8b5d679774a682d98195c2cfa782aa6585588
Nabil Affo <nabil.noble01@gmail.com>
2022-03-15 17:04:29 +0000

    Update script 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,9 +7,9 @@ void times_table(void)
 {
 	int i, j, n;
 
-	for (i = 0, i < 10, i++)
+	for (i = 0; i < 10; i++)
 	{
-		for (j = 0, j < 10, j++)
+		for (j = 0; j < 10; j++)
 		{
 			n = i * j;
 			if (j == 0)
