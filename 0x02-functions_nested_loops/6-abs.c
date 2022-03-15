614c25d4dd3c761418c14c4ef3550cbf545347e3
Nabil Affo <nabil.noble01@gmail.com>
2022-03-15 14:21:47 +0000

    Write script 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute value of an integer
+ * @n: number to pass as argument of the function
+ * Return: the absolute value
+ */
+int _abs(int n)
+{
+
+	if (n < 0)
+	{
+		return (-n);
+	}
+	else
+		return (n);
+}
