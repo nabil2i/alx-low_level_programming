e2eb3e893d033e01fe828a35ef3f02deb1aa1fd2
Nabil Affo <nabil.noble01@gmail.com>
2022-03-15 14:26:22 +0000

    Write script 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * print_last_digit - prints the last digit of the number n
+ * @n: number to pass as argument
+ * Return: the last digit
+ */
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
