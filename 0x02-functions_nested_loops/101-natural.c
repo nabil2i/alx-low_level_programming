f5d046944d912324e6e1652f4af3d56a244a6331
Nabil Affo <nabil.noble01@gmail.com>
2022-03-15 17:52:19 +0000

    Write script 13

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - compute and print the sum of all multiples
+ * of 3 and 5 below 1024 (excluded)
+ * Return: 
+ */
+int main(void)
+{
+	int i, n;
+	
+	n =0;
+	for (i = 0; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 ==0)
+		{
+			n = n + i;
+		}
+	}
+	printf("%d\n", n);
+	return (0);
+}
