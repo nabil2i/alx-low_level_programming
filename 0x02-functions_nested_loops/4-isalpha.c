12c8850dcbe9176bd0586d003072f63f1b1f045b
Nabil Affo <nabil.noble01@gmail.com>
2022-03-15 12:25:28 +0000

    Update script 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- * isalpha - checks for alphabetic characters
+ * _isalpha - checks for alphabetic characters
  * Return: 1 if character is a letter lowercase or uppercase
  * and 0 otehrwise
  * @c: argument to check
- * */
+ */
 int _isalpha(int c)
 {
-	if ((c > 96 && c < 123) || (c > 64 && c < 91 ))
+	if ((c > 96 && c < 123) || (c > 64 && c < 91))
 		return (1);
 	else
 		return (0);
