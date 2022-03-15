daca5a8000b399f6cf7e2ac58232812f7d98b3aa
Nabil Affo <nabil.noble01@gmail.com>
2022-03-15 16:02:06 +0000

    Write script 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print natural numbers from n to 98
+ * @n: argument
+ * Return: Nothing
+ */
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		while (n < 99)
+		{
+			printf("%d", n)
+			if (n != 98)
+			{
+				printf(", ");
+			}
+			n++;
+		}
+	}
+	else if (n > 98)
+	{
+		while (n > 98)
+		{
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
+			n--;
+		}
+	}
+}
