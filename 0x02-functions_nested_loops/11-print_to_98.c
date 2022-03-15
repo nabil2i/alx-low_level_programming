68afcdfc75c7b80e416e17893b52befd8efc7aaa
Nabil Affo <nabil.noble01@gmail.com>
2022-03-15 16:09:25 +0000

    Update script 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,8 +6,8 @@
  * Return: Nothing
  */
 void print_to_98(int n)
-{
-	if (n < 98)
+{i
+	if (n <= 98)
 	{
 		while (n < 99)
 		{
@@ -16,17 +16,25 @@ void print_to_98(int n)
 			{
 				printf(", ");
 			}
+			else if (n == 98)
+			{
+				printf("\n");
+			}
 			n++;
 		}
 	}
 	else if (n > 98)
 	{
-		while (n > 98)
+		while (n >= 98)
 		{
 			printf("%d", n);
 			if (n != 98)
 			{
 				printf(", ");
+			}
+			else if (n == 98)
+			{
+				printf("\n");
 			}
 			n--;
 		}
