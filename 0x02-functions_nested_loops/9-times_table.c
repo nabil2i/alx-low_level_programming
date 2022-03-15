5bf120b6384b985f9d75b015da4b15a9e776e94a
Nabil Affo <nabil.noble01@gmail.com>
2022-03-15 15:25:51 +0000

    Write script 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,42 @@
+#include "main.h"
+/**
+ * times_table - prints the 9 times table
+ * Return: nothing
+ */
+void times_table(void)
+{
+	int i, j, n;
+
+	i = 0;
+	while (i < 10)
+	{
+		j = 0;
+		while (j < 10)
+		{
+			n = i * j;
+			if (n < 10)
+			{
+				_putchar(n + 48);
+				if (j < 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+				j++;
+			}
+			else if (n >= 10)
+			{
+				_putchar((n / 10) + 48);
+				_putchar((n % 10) + 48);
+				if (j < 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+				j++;
+			}
+		}
+		_putchar('\n');
+		i++;
+	}
+}
