85f9aeb9ad8e87603c094d52389ff9aceeb33f2e
Nabil Affo <nabil.noble01@gmail.com>
2022-03-15 15:49:11 +0000

    Updatee script 10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,9 +1,11 @@
 #include "main.h"
 /**
  * add - adds two integers
+ * @n: first argument
+ * @m: second argument
  * Return: result of addition
  */
 int add(int n, int m)
 {
-	return(n+m);
+	return (n+m);
 }
