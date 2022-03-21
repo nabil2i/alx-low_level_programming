ebdf2d2f3521f45c6429a129db98bdb19d666c10
Nabil Affo <nabil.noble01@gmail.com>
2022-03-21 12:11:08 +0000

    Update main.h and write script 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.c"
+
+/**
+ * reset_to_98 - takes a pointer to an int as parameter and
+ * updates the value it points to to 98
+ * Return: nothing
+ */
+void reset_to_98(int *n);
+{
+	*n = 98;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,4 +1,5 @@
 /**
  * Library of functions
  */
-int _putchar(char c)
+int _putchar(char c);
+void reset_to_98(int *n);
