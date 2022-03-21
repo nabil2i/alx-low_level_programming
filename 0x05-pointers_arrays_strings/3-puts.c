008fefe5eb511bf3095fea4f77972a1fd0db9bb0
Nabil Affo <nabil.noble01@gmail.com>
2022-03-21 12:54:00 +0000

    Write script 3wq

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string
+ * @str: arg, pointer to a string
+ * Return: nothing
+ */
+void _puts(char *str)
+{
+	int i;
+
+	i = 0;
+	while (*(str + i) != '\0')
+	{
+		_putchar(*(str + i));
+		i++;
+	}
+	_putchar('\n');
+}
