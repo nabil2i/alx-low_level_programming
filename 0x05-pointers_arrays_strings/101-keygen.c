aae6648f1409f119935494374aeb8bfc4eefb8fc
Nabil Affo <nabil.noble01@gmail.com>
2022-03-21 23:06:26 +0000

    Write script 11

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - generates randome valid password
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	srand(time(NULL));
+
+
+	return (0);
+}
