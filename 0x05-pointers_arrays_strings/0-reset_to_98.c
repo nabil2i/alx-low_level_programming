8b9a1675db47b103a724b5f4188bbc3b24593917
Nabil Affo <nabil.noble01@gmail.com>
2022-03-21 12:15:44 +0000

    Update script 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -4,8 +4,9 @@
  * reset_to_98 - takes a pointer to an int as parameter and
  * updates the value it points to to 98
  * Return: nothing
+ * @n: pointer to an integer
  */
-void reset_to_98(int *n);
+void reset_to_98(int *n)
 {
 	*n = 98;
 }
