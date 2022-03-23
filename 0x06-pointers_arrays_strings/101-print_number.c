bdd68a9b2cf3023c695b673803d39871e02bf4bd
Nabil Affo <nabil.noble01@gmail.com>
2022-03-23 18:55:53 +0000

    Write script 9

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ * @n: arg, integer to be printed
+ * Return: nothing
+ */
+void print_number(int n)
+{
+	unsigned int m;
+
+	if (n < 0)
+	{
+		m = -n;
+		_putchar('-');
+	}
+	else
+	{
+		m = n;
+	}
+	if (m / 10 != 0)
+	{
+		print_number(m / 10);
+	}
+	_putchar((m % 10) + 48);
+}
