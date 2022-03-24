b270584bff3b8dd97d6589f3de7aa2182f30fa93
Nabil Affo <nabil.noble01@gmail.com>
2022-03-24 15:10:04 +0000

    Update 100

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,31 +7,33 @@
  */
 int _atoi(char *s)
 {
-	int i, d, n, len, flag, digit;
+	int i, posneg, k, n,  len, flag;
 
 	i = 0;
-	d = 0;
+	posneg = 0;
 	n = 0;
 	len = 0;
 	flag = 0;
-	digit = 0;
+	k = 0;
 
 	while (s[len] != '\0')
+	{
 		len++;
+	}
 
 	while (i < len && flag == 0)
 	{
 		if (s[i] == '-')
-			++d;
+			++posneg;
 
-		if (s[i] >= '0' && s[i] <= '9')
+		if (s[i] >= 48 && s[i] <= 57)
 		{
-			digit = s[i] - '0';
-			if (d % 2)
-				digit = -digit;
-			n = n * 10 + digit;
-			f = 1;
-			if (s[i + 1] < '0' || s[i + 1] > '9')
+			k = s[i] - '0';
+			if (posneg % 2)
+				k = -k;
+			n = n * 10 + k;
+			flag = 1;
+			if (s[i + 1] < 48 || s[i + 1] > 57)
 				break;
 			flag = 0;
 		}
