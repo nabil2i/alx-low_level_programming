44d626875dd9464eb1e9d47b6453ace8865c6731
Nabil Affo <nabil.noble01@gmail.com>
2022-03-25 21:04:06 +0000

    Update script 11

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * infinite_add - adds two numbers
  * @n1: arg1, first number
@@ -10,55 +9,56 @@
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i, j, k, len1, len2, n, m, digit, result;
+	int i, j, k, l, reminder, a[255], b[255], sum[255];
 
 	i = 0;
 	j = 0;
-	len1 =0;
-	len2 = 0;
-	n = 0;
-	m = 0;
-	digit = 0;
-	result = 0;
 	k = 0;
-	while (n1[len1] != '\0')
+	l = 0;
+	reminder = 0;
+	while (*n1)
+		a[i++] = *n1++ - 48;
+	while (*n2)
+		b[j++] = *n2++ - 48;
+	if (i < j)
 	{
-		len1++;
-	}
-	while (n1[len2] != '\0')
-	{
-		len2++;
-	}
-	while (i < len1)
-	{
-		digit = n1[i] - '0';
-		n = n * 10 + digit;
-		i++;
-	}
-	while (j > len2)
-	{
-		digit = n2[j] - '0';
-		m = m * 10 + digit;
-		j++;
-	}
-	result = n + m;
-	while (result != 0)
-	{
-		result = result / 10;
-		++k;
-	}
-	if (k > size_r)
-	{
-		return ("Error");
+		for (l = i; l > 0; l--)
+		{
+			sum[k++] = ((a[l - 1] + b[--j]) + reminder) % 10;
+			reminder = ((a[l - 1] + b[j]) + reminder) / 10;
+		}
+		while (j > 0)
+		{
+			sum[k++] = b[--j] + reminder;
+			reminder = 0;
+		}
 	}
 	else
 	{
-		while (result != 0)
+		for (l = j; l > 0; l--)
 		{
-			int a = result % 10;
-			result = result / 10;
-			r[k--] = a + '0';
+			sum[k++] = ((b[l - 1] + a[--i]) + reminder) % 10;
+			reminder = ((b[l - 1] + a[i]) + reminder) / 10;
 		}
+		if (i == 0 && reminder == 1)
+			sum[k++]  = reminder;
+		while (i > 0)
+		{
+			sum[k++] = a[--i] + reminder;
+			reminder = 0;
+		}
+	}
+	if (k + 2 > size_r)
+	{
+		return (0);	
+	}
+	else
+	{
+		for (l = k - 1; l >= 0; l--)
+		{
+			r[k - l - 1] = sum[l] + '0';
+		}	 
+		r[k] = '\0';
 		return (r);
 	}
 }
