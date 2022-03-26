dd9fded9318a8e1a5c46e9d31fc0110b86f30a1f
Nabil Affo <nabil.noble01@gmail.com>
2022-03-26 15:08:53 +0000

    Updatete script 12

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,52 +1,47 @@
 #include "main.h"
 #include <stdio.h>
-#include <unistd.h>
+
 /**
- * _putchar - function the prints a character given as arg
- * Return: 1 (Success)
- * Return: Negative number (Error)
+ * printline - prints s bytes buffer
+ * @c: buffer
+ * @s: bytes of buffer
+ * @l: line of buffer 
+ * Return: Nothing
  */
-int _putchar(char c)
+
+void printline(char *c, int s, int l)
 {
-	return(write(1, &c, 1));
-}
-/**
- * prinline - prints a bytes on a line
- * @s: buffer
- * @l: line
- */
-void printline(char *s, int a, int l)
-{
-	int i, j;
-	for (i = 0; i <= 9; i++)
+	int j, k;
+
+	for (j = 0; j <= 9; j++)
 	{
-		if (i <= a)
-			printf("%02x", s[l * 10 + i]);
-		else 
-			printf("  ");
-		if (i % 2 != 0)
-			_putchar(' ');
-	}
-	for (j = 0; j <= a; j++)
-	{
-		if (s[l * 10 + j] > 31 && s[l * 10 + j] < 127)
-			_putchar(s[l * 10 + j]);
+		if (j <= s)
+			printf("%02x", c[l * 10 + j]);
 		else
-			_putchar('.');
+			printf("  ");
+		if (j % 2)
+			putchar(' ');
+	}
+	for (k = 0; k <= s; k++)
+	{
+		if (c[l * 10 + k] > 31 && c[l * 10 + k] < 127)
+			putchar(c[l * 10 + k]);
+		else
+			putchar('.');
 	}
 }
 
 /**
  * print_buffer - prints a buffer
- * @b: arg1, buffer to be printed
- * @size: 
+ * @b: buffer
+ * @size: size of buffer
  * Return: Nothing
  */
 void print_buffer(char *b, int size)
 {
-
 	int i;
-	for (i = 0; i <= (size - 1) / 10 && size != 0; i++)	
+
+	for (i = 0; i <= (size - 1) / 10 && size; i++)
 	{
 		printf("%08x: ", i * 10);
 		if (i < size / 10)
@@ -57,9 +52,8 @@ void print_buffer(char *b, int size)
 		{
 			printline(b, size % 10 - 1, i);
 		}
-		_putchar('\n');
+		putchar('\n');
 	}
 	if (size == 0)
-		_putchar('\n');
-
+		putchar('\n');
 }
