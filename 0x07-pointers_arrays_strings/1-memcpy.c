9a4dbd145364c3095879f2fd3590b9aedddf29ce
Nabil Affo <nabil.noble01@gmail.com>
2022-03-28 13:51:57 +0000

    Write script 1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memcpy - copyies n bytes from memory
+ * @dest: pointer to where n bytes are copied to
+ * @src: pointer to where n bytes are copied from
+ * @n: bytes to copy
+ * Return: pointer to destination
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
