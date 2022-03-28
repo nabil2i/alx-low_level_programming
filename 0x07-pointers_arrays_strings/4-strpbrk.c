352ca9408d88af7443614447efdbabd02652a30a
Nabil Affo <nabil.noble01@gmail.com>
2022-03-28 15:17:25 +0000

    Write script 4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strpbrk - searches a string for any of a set of bytes
+ * @s: pointer to a string
+ * @accept: pointer to substring
+ * Return: pointer to the first occurence of a bytes in accept
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+				return (s+i);
+		}
+	}
+	return (NULL);
+
+}
