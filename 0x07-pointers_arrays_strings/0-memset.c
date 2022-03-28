3a55ab4a80761a67a45e1a4031199805c27ce276
Nabil Affo <nabil.noble01@gmail.com>
2022-03-28 13:37:24 +0000

    Write script 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -4,6 +4,7 @@
  * @s:arg1, pointer to a string
  * @b:arg2, constant byte for memory filling
  * @n: arg3, bytes filled in memory
+ * Return: pointer to s
  */
 char *_memset(char *s, char b, unsigned int n)
 {
