93715f0207ee94698f911d8aae7351bf985cc7dc
Nabil Affo <nabil.noble01@gmail.com>
2022-03-30 21:48:06 +0000

    Write 101

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * wildcmp - compares two strings
+ * @s1: string #1
+ * @s2: string #2
+ * return: 1 if strings are equal, 0 if not
+ */
+int wildcmp(char *s1; char *s2)
+{
+	
+}
