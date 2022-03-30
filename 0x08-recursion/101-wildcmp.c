dca53560f7f9caff747db66f09867530f7e872b7
Nabil Affo <nabil.noble01@gmail.com>
2022-03-30 22:37:49 +0000

    Update 101

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -16,34 +16,24 @@ int strlen_rec(char *s)
  * wildcmp - compares two strings
  * @s1: string #1
  * @s2: string #2
- * return: 1 if strings are equal, 0 if not
+ * Return: 1 if strings are equal, 0 if not
  */
 int wildcmp(char *s1, char *s2)
 {
-	/*int len1, len2;
 
-	len1 = strlen_rec(s1);
-	len2 = strlen_rec(s2);*/
-	
-	/*if (*s1 == ' ')
+	if (*s1 == ' ')
 		return (wildcmp(s1 + 1, s2));
 	if (*s2 == ' ')
-		return (wildcmp(s1, s2 + 1));*/
+		return (wildcmp(s1, s2 + 1));
 
 	if (*s1 == '\0' || *s2 == '\0')
 		return (0);
 
 	if (*s2 == '*')
 		return (wildcmp(s1, s2 + 1));
-	
-	
+
 	if (*s1 == '\0' && *s2 == '\0')
 		return (1);
-
-	/*if (*s1 == '\0')
-		return (len1);
-	if (*s2 == '\0')
-		return (len2);*/
 
 	if (*s1 == *s2)
 	{
