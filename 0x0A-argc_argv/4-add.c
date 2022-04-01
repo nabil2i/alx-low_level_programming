168800875a2e2ec97a5c87cf8a560f5a32cbe787
Nabil Affo <nabil.noble01@gmail.com>
2022-04-01 10:23:06 +0000

    Update 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int checkdigit(char c);
+
+/**
+ * checkdigit - checks if the charcater is a digit or not
+ * @c: character to check
+ * Return: 1 if it is a digit and 0 otherwiwse
+ */
+int checkdigit(char c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	else
+		return (0);
+}
+
 /**
  * main - adds to positive numbers
  * @argc: count of the arguments supplied to the program
@@ -9,12 +24,25 @@
  */
 int main(int argc, char **argv)
 {
-	int i, sum;
+	int i, j, sum;
 
 	sum = 0;
 	if (argc < 1)
 	{
 		printf("0\n");
+	}
+	for (i = 1; i < argc; i++)
+	{
+		j = 0;
+		while (argv[i][j] != '\0')
+		{
+			if (!checkdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+			j++;
+		}
 	}
 	for (i = 1; i < argc; i++)
 	{
