c579e108c831afbedb32e30f4b2048456177294a
Nabil Affo <nabil.noble01@gmail.com>
2022-04-13 12:20:06 +0000

    Update 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -6,12 +6,8 @@
  * @f: pointer to a function
  * Return: Nothing
  */
-
 void print_name(char *name, void (*f)(char *))
 {
-	if (name != NULL || f != NULL)
+	if (name != NULL && f != NULL)
 		f(name);
-	else
-		return;
-
 }
