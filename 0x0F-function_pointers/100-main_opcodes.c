158e3f50e8871694e5f6f624cb97011a2de2ef16
Nabil Affo <nabil.noble01@gmail.com>
2022-04-13 15:58:19 +0000

    Write 100

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the opcodes of its own main function
+ * @argc: number of arguments passed to main program
+ * @argv: pointer to an array of char
+ * Return: Always 0 (Success°
+ */
+int main(int argc, char *argv[])
+{
+	int number_bytes, i;
+	char *instruction = (char *)main;
+	
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	number_bytes = atoi(argv[1]);
+	if (number_bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	for (i = 0; i < number_bytes; i++)
+	{
+		if (i == number_bytes - 1)
+		{
+			printf("%.2hhx\n", instruction[i]);
+		}
+		
+		printf("%.2hhx ", instruction[i]);
+	}
+
+	return (0);
+}
