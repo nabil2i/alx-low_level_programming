55dc1e559919c306077c25c0e43ea6483fe646bc
Nabil Affo <nabil.noble01@gmail.com>
2022-04-14 09:31:05 +0000

    Create variadic_functions.h and .gitignore files

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,10 @@
+#ifndef VAR_FUNC
+#define VAR_FUNC
+
+int _putchar(char);
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif /* VAR_FUNC */
