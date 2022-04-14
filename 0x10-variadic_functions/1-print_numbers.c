c4e8239a1c1cca577110285e84f4b69b3a21116c
Nabil Affo <nabil.noble01@gmail.com>
2022-04-14 10:26:33 +0000

    Update 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -19,15 +19,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		return;
 	for (i = 0; i < n; i++)
 	{
-		if (i == n - 1)
-		{
-			printf("%d\n", va_arg(ap, int));
-			break;
-		}
-		if (separator == NULL)
-			printf("%d", va_arg(ap, int));
-		else
-			printf("%d%s", va_arg(ap, int), separator);
+		printf("%d", va_arg(ap, int));
+		if (separator != NULL && i < n - 1)
+			printf("%s", separator);
 	}
+	printf("\n");
 	va_end(ap);
 }
