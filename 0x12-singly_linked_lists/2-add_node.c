162329d1da68e8fa5732aee4b502f047e8a32b0b
Nabil Affo <nabil.noble01@gmail.com>
2022-04-21 13:18:40 +0000

    Update 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -24,7 +24,9 @@ list_t *add_node(list_t **head, const char *str)
 		new_node->len = len;
 		new_node->next = (*head);
 		(*head) = new_node;
+		return (*head);
 	}
-	return (*head);
+	return (NULL);
+
 
 }
