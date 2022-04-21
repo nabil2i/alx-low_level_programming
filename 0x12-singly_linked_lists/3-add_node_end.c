3c28ebcd17d67d43911f68c0e0d03e61c40a06d0
Nabil Affo <nabil.noble01@gmail.com>
2022-04-21 13:13:37 +0000

    Update 3

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -27,7 +27,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	if (*head == NULL)
 	{
 		*head = new_node;
-		return (*head);
+		return (new_node);
 	}
 
 	while (last->next != NULL)
@@ -35,7 +35,7 @@ list_t *add_node_end(list_t **head, const char *str)
 		last = last->next;
 	}
 	last->next = new_node;
-	return (*head);
+	return (new_node);
 
 
 }
