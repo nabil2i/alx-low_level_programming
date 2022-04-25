130aa83db60467603560e56db1a83ebc617b3f9d
Nabil Affo <nabil.noble01@gmail.com>
2022-04-25 16:48:45 +0000

    Update 7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -12,7 +12,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	unsigned int i;
 
 	cursor = head;
-	for (i = 0; i <= index; i++)
+	for (i = 0; i < index; i++)
 	{
 		cursor = cursor->next;
 	}
