95c72c7c79d1711631fe7f54c9b38559ed8a6b9e
Nabil Affo <nabil.noble01@gmail.com>
2022-04-26 21:09:31 +0000

    101, 102, 103

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -7,29 +7,22 @@
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t nodes;
+	size_t num = 0;
 	long int diff;
 
-	if (!head)
-		exit(98);
-
-	nodes = 0;
 	while (head)
 	{
-		printf("[%p] %d\n", (void *)head, head->n);
-		head = head->next;
-		nodes++;
-
 		diff = head - head->next;
+		num++;
+		printf("[%p] %d\n", (void *)head, head->n);
 		if (diff > 0)
-		{
 			head = head->next;
-		}
 		else
 		{
 			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
 			break;
 		}
 	}
-	return (nodes);
+
+	return (num);
 }
