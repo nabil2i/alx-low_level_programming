28c4a2cec764eef17eebbdc4d0438390af8395b8
Nabil Affo <28634540+nabil2i@users.noreply.github.com>
2022-06-14 01:17:57 +0000

    Update 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/nabil2i/alx-low_level_programming/blob/185ef2714204feb691ce4948bf6c01dca5e23522/0x18-dynamic_libraries/libinj.so
+wget -P /tmp https://github.com/nabil2i/alx-low_level_programming/blob/58d1ed0b14dc2cec8501400f566eb0d7b5747107/0x18-dynamic_libraries/libinj.so
 export LD_PRELOAD=/tmp/libinj.so
