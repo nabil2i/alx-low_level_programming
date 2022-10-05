4a3e4a1d5e1b683a33a7a5b8a7046ee17bc45e5d
Nabil <nabil.noble01@gmail.com>
2022-10-05 20:19:20 +0000

    Edit

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,11 +1,10 @@
 #include "search_algos.h"
 
 /**
- * print_search_array - prints the array of integers
- * to be search
+ * print_search_array - print the array that is being
+ * seach into
  * @array: pointer to the array to print
- * @size: size of array
- *
+ * @size: size of the array
  * Return: nothing
  */
 void print_search_array(int *array, size_t size)
@@ -21,37 +20,38 @@ void print_search_array(int *array, size_t size)
 	}
 	printf("\n");
 }
-
 /**
- * binary_search - searches for a value in a sorted array
- * of integers using Binary search algorithm
- * @array: pointer to the array to search into
- * @size: size  of the array
- * @value: value to search for
+ * binary_search - searches for a value in an array
+ * if integers using the  algorithm
+ * @array: pointer to the first element of the array to search in
+ * @size: number of elements in array
+ * @value: value to search
  *
- * Return: index where value is located, or -1 if
- * the value is not found
+ * Return: first index where value is located
+ * or -1 if value is not present or if array NULL
  */
 int binary_search(int *array, size_t size, int value)
 {
-	size_t start, middle, end;
+	size_t start, end, middle;
 
-	if (array != NULL && size > 0)
+	if (!array || size == 0)
 	{
-		start = 0;
-		end = size - 1;
+		return (-1);
+	}
+
+	start = 0;
+	end = size - 1;
+	print_search_array(array + start, end + 1 - start);
+	while (start < end)
+	{
+		middle = (start + end) / 2;
+		if (array[middle] < value)
+			start = middle + 1;
+		else if (array[middle] > value)
+			end = middle - 1;
+		else
+			return (middle);
 		print_search_array(array + start, end + 1 - start);
-		while (start < end)
-		{
-			middle = (start + end) / 2;
-			if (array[middle] == value)
-				return (middle);
-			else if (array[middle] < value)
-				start = middle + 1;
-			else
-				end = middle - 1;
-			print_search_array(array + start, end + 1 - start);
-		}
 	}
 	return (-1);
 }
