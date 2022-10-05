fd543dfecdcb89d07b2860741cdb735131ad472d
Nabil <nabil.noble01@gmail.com>
2022-10-05 19:09:23 +0000

    Edit

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -42,6 +42,7 @@ typedef struct skiplist_s
 
 /* FUNCTIONS PROTOTYPES */
 int linear_search(int *array, size_t size, int value);
+void print_search_array(int *array, size_t size);
 int binary_search(int *array, size_t size, int value);
 
 int jump_search(int *array, size_t size, int value);
